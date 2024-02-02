b661bc88aca4a8c72b66a0d17b27fc67acbfbcaf
seout <113185077+seout@users.noreply.github.com>
2024-02-02 15:45:39 +0300

    Update ModeLabel.py

--- src/ModeLabel.py
+++ src/ModeLabel.py
@@ -16,28 +16,32 @@ class ModeLabel(tkinter.Label):
         self.setrndm       = random.randrange(len(src.sortlist.GetArray));
 
         super().__init__(self.master,
-                                         text=src.sortlist.word[self.setrndm],
+                                         text=src.sortlist.word[self.setrndm],bg="black",
                                                         pady=55,font=("Arial", 45), fg="white");
         self.viewPronunciation = tkinter.Label(self.master,
-                                         text=src.sortlist.prnce[self.setrndm], fg="red", font=("Helvetica", 20));
+                                         text=src.sortlist.prnce[self.setrndm], fg="red", font=("Helvetica", 20),bg="black",);
+
         self.viewExample = tkinter.Label(self.master,
-                                         text="Example: " + src.sortlist.exmpl[self.setrndm] , fg="red", pady=250);
+                                         text="Example: " + src.sortlist.exmpl[self.setrndm] , fg="red",bg="black",font=("Helvetica", 15));
         self.viewTranslate = tkinter.Label(self.master,
-                                         text=src.sortlist.trnslte[self.setrndm], pady=25,font=("Arial", 45), fg="red");
+                                         text=src.sortlist.trnslte[self.setrndm], pady=55,font=("Arial", 45), fg="white",bg="black",);
 
     def update(self):
 
         super().place_forget()
         self.viewPronunciation.place_forget()
-
-        self.viewTranslate.pack(padx=5, pady=10);
-        self.viewExample.place(x=75, y=75, width=250, height=95);
+        self.x = self.master.winfo_screenmmwidth()
+        self.y = self.master.winfo_screenmmheight()
+        self.viewTranslate.place(width=self.x-65, height=50,x=0, y=45);
+ #.pack(padx=5, pady=10);
+        self.viewExample.place(x=110, y=110);
+# place(x=75, y=75, width=250, height=95);
 
         self.turnstatus = 1;
-        time.sleep(6)
+        time.sleep(1)
 
         self.master.withdraw()
-        time.sleep(self.msec)
+        time.sleep(1)
 
         return self.appear();
 
@@ -51,12 +55,12 @@ class ModeLabel(tkinter.Label):
             super().place(x=100, y=-20);
             self.viewPronunciation.place(x=155, y=100);
 
-            timein = threading.Timer(10, self.update);
+            timein = threading.Timer(1, self.update);
             timein.start();
 
         else:
 
-            self.viewTranslate.pack_forget();
+            self.viewTranslate.place_forget();
             self.viewExample.place_forget()
             self.setrndm = random.randrange(len(src.sortlist.GetArray));
             self.turnstatus = 0;
