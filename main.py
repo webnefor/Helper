f9ae9ff33905ba6c0da144a2f9d9d81393250530
seout <113185077+seout@users.noreply.github.com>
2024-02-02 15:08:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#
+# https://github.com/seout
+#
+
+import src.CoreTk
+import src.ModeLabel
+import src.sortlist
+
+from src.args import getparse, mintosec
+
+def main() -> int:
+    rslt = getparse()
+    #
+    src.sortlist.init(rslt.path);
+    #
+    core = src.CoreTk.Gpeg();
+    #
+    view = src.ModeLabel.ModeLabel(core, mintosec(rslt.time));
+    #
+    view.create()
+    #
+    view.appear()
+    #
+    core.start();
+    #
+
+    return 0;
+
+if __name__ == "__main__":
+    main()
